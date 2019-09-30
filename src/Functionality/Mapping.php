33be671b31d1fc5f1b865a6cf4af8d55966320a3
HDVinnie <unit3d@protonmail.com>
2019-09-30 13:19:37 -0400

    (Update) TemplateShares Package

---
+++ src/Functionality/Mapping.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace HDVinnie\TemplateSharesToUnit3d\Functionality;
+
+use stdClass;
+use Carbon\Carbon;
+
+class Mapping
+{
+    /**
+     * @param  string  $type
+     * @param  stdClass  $data
+     * @return array
+     */
+    public static function map(string $type, stdClass $data): array
+    {
+        return self::{'map'.$type}($data);
+    }
+
+    /**
+     * @param  stdClass  $data
+     * @return array
+     */
+    public static function mapGroup(stdClass $data): array
+    {
+        // TODO
+    }
+
+    /**
+     * @param  stdClass  $data
+     * @return array
+     */
+    public static function mapUser(stdClass $data): array
+    {
+        return [
+            'username' => $data->username,
+            'password' => $data->passhash ?? null,
+            'passkey' => $data->secret ?? md5(uniqid().time().microtime()),
+            'rsskey' => $data->secret ?? md5(uniqid().time().microtime().$data->passhash),
+            'group_id' => 3, // Default Member Group
+            'email' => $data->email ?? null,
+            'uploaded' => $data->uploaded ?? 0,
+            'downloaded' => $data->downloaded ?? 0,
+            'seedbonus' => $data->seedbonus ?? 0,
+            'image' => $data->avatar ?? null,
+            'title' => $data->title ?? null,
+            'about' => $data->about ?? null,
+            'signature' => $data-signature ?? null,
+            'active' => $data->enabled ?? true,
+            'invites' => $data->invites ?? 0,
+            'last_login' => Carbon::createFromTimeString($data->last_login),
+            'created_at' => Carbon::createFromTimeString($data->added),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+
+    /**
+     * @param  stdClass  $data
+     * @return array
+     */
+    public static function mapCategory(stdClass $data): array
+    {
+        // TODO
+    }
+
+    /**
+     * @param  stdClass  $data
+     * @return array
+     */
+    public static function mapTorrent(stdClass $data): array
+    {
+        // TODO
+    }
+}
